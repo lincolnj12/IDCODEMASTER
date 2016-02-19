a56596a93b2d8cadb76285d4ca4e1ed01a00b84f
FioGalvan <galvansotofiorella@gmail.com>
2016-02-19 18:09:11 -0500

    PAGO DAO

--- WcfService/Persistencia/PagoPendienteDAO.cs
+++ WcfService/Persistencia/PagoPendienteDAO.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using WcfService.Dominio;
-
+using System.Data.SqlClient;
 namespace WcfService.Persistencia
+
 {
-    public class PagoDAO:BaseDAO<Pago,int>
+    public class PagoPendienteDAO
     {
     }
 }
