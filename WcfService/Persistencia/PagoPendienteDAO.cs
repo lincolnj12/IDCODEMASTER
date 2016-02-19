8e3a7682c537a6337b3cc5ffc4e3a37bc17d9601
FioGalvan <galvansotofiorella@gmail.com>
2016-02-19 18:09:56 -0500

    DAO

--- WcfService/Persistencia/PagoPendienteDAO.cs
+++ WcfService/Persistencia/PagoPendienteDAO.cs
@@ -5,9 +5,44 @@ using System.Web;
 using WcfService.Dominio;
 using System.Data.SqlClient;
 namespace WcfService.Persistencia
-
 {
-    public class PagoPendienteDAO
+    public class PagoPendienteDAO : BaseDAO<PagoPendiente, int>
     {
+        public List<PagoPendiente> ListarTodos(string codAlumno)
+        {
+            List<PagoPendiente> listaPagosPendientes = new List<PagoPendiente>();
+
+            string sql = "select cd_alumno, ds_pago, qt_monto, dt_fecha where cd_alimno=@cod";
+
+
+
+            using (SqlConnection con = new SqlConnection(ConexionUtil.ObtenerCadena()))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@cod", codAlumno));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        while (resultado.Read())
+                        {
+                            PagoPendiente pagoPendiente = new PagoPendiente()
+                            {
+
+                                cd_alumno = (string)resultado["cd_alumno"],
+                                ds_pago = (string)resultado["ds_pago"],
+                                qt_monto = (double)resultado["qt_monto"],
+                                dt_fecha = (DateTime)resultado["dt_fecha"],
+
+
+                            };
+                            listaPagosPendientes.Add(pagoPendiente);
+                        }
+                    }
+                }
+            }
+            return listaPagosPendientes;
+        }
+
     }
 }
