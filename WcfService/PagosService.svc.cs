fe1985929eb0f7e6f8332793b1d7ed9fadc77f20
FioGalvan <galvansotofiorella@gmail.com>
2016-02-19 17:58:00 -0500

    Pagos Services

---
+++ WcfService/PagosService.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService.Dominio;
+using WcfService.Persistencia;
+
+namespace WcfService
+{
+    public class PagosService : IPagosService
+    {
+        private PagoPendienteDAO dao = new PagoPendienteDAO();
+
+        public List<PagoPendiente> ListarPagosPendientesAlumno(string codAlumno)
+        {
+            int cantidadPagosPendientes = dao.ListarTodos(codAlumno).Count();
+
+
+            if (cantidadPagosPendientes <= 0)
+            {
+                throw new WebFaultException<string>(
+                        "El alumno no tiene deuda pendiente.", HttpStatusCode.InternalServerError);
+            }
+
+            return dao.ListarTodos(codAlumno);
+        }
+    }
+}
